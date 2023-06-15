46f1c2eb669e8faace4bb11551677e44d7342f66
Meloman <107422136+melomanexe@users.noreply.github.com>
2023-06-15 15:01:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-while True:
-    print('a')
+def is_palindrome(string):
+    n = len(string)
+    for i in range(n // 2):
+        if string[i] != string[n - i - 1]:
+            return False
+    return True
